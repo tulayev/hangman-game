1720c79bdc3a7123439afc812496e27d99c84971
Umarbek <umarbek@mail.uz>
2021-09-27 11:34:22 -0700

    first commit

---
+++ Viselka/FormViselka.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Viselka
+{
+    partial class FormViselka
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureStep = new System.Windows.Forms.PictureBox();
+            this.labelWord = new System.Windows.Forms.Label();
+            this.panelKeys = new System.Windows.Forms.Panel();
+            this.textList = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureStep)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureStep
+            // 
+            this.pictureStep.BackColor = System.Drawing.Color.White;
+            this.pictureStep.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureStep.Image = global::Viselka.Properties.Resources.step0;
+            this.pictureStep.Location = new System.Drawing.Point(3, 9);
+            this.pictureStep.Name = "pictureStep";
+            this.pictureStep.Size = new System.Drawing.Size(247, 240);
+            this.pictureStep.TabIndex = 0;
+            this.pictureStep.TabStop = false;
+            // 
+            // labelWord
+            // 
+            this.labelWord.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelWord.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelWord.Location = new System.Drawing.Point(256, 9);
+            this.labelWord.Name = "labelWord";
+            this.labelWord.Size = new System.Drawing.Size(424, 74);
+            this.labelWord.TabIndex = 1;
+            this.labelWord.Text = "*****";
+            this.labelWord.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelWord.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.labelWord_MouseDoubleClick);
+            // 
+            // panelKeys
+            // 
+            this.panelKeys.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelKeys.Location = new System.Drawing.Point(256, 86);
+            this.panelKeys.Name = "panelKeys";
+            this.panelKeys.Size = new System.Drawing.Size(424, 163);
+            this.panelKeys.TabIndex = 2;
+            // 
+            // textList
+            // 
+            this.textList.Font = new System.Drawing.Font("Lucida Console", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textList.Location = new System.Drawing.Point(3, 9);
+            this.textList.Multiline = true;
+            this.textList.Name = "textList";
+            this.textList.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textList.Size = new System.Drawing.Size(247, 240);
+            this.textList.TabIndex = 3;
+            this.textList.Visible = false;
+            // 
+            // FormViselka
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 254);
+            this.Controls.Add(this.textList);
+            this.Controls.Add(this.panelKeys);
+            this.Controls.Add(this.labelWord);
+            this.Controls.Add(this.pictureStep);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MinimizeBox = false;
+            this.Name = "FormViselka";
+            this.Text = "Игра Виселка";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureStep)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureStep;
+        private System.Windows.Forms.Label labelWord;
+        private System.Windows.Forms.Panel panelKeys;
+        private System.Windows.Forms.TextBox textList;
+    }
+}
+
---
+++ Viselka/FormViselka.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Viselka
+{
+    public partial class FormViselka : Form
+    {
+        private string abc = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+        private string word;
+        private int errors;
+        private Button[] buttonKeys;
+        private static Random random = new Random();
+
+        public FormViselka()
+        {
+            InitializeComponent();
+            InitKeys();
+            InitGame();
+        }
+
+        private void InitKeys()
+        {
+            buttonKeys = new Button[abc.Length];
+            int x, y;
+            int buttonWidth = panelKeys.Width / 11;
+            int buttonHeight = panelKeys.Height / 3;
+            for (int i = 0; i < abc.Length; i++)
+            {
+                buttonKeys[i] = new Button();
+                x = i % 11;
+                y = i / 11;
+                buttonKeys[i].Location = new Point(x * buttonWidth, y * buttonHeight);
+                buttonKeys[i].Size = new Size(buttonWidth - 2, buttonHeight - 4);
+                buttonKeys[i].TabIndex = i;
+                buttonKeys[i].Tag = i;
+                buttonKeys[i].Text = abc.Substring(i, 1);
+                buttonKeys[i].Click += new EventHandler(this.button_Click);
+                panelKeys.Controls.Add(buttonKeys[i]);
+            }
+        }
+
+        private void InitGame()
+        {
+            errors = 0;
+            for (int i = 0; i < abc.Length; i++)
+            {
+                buttonKeys[i].Visible = true;
+            }
+            ChooseWord();
+            labelWord.Text = new String('#', word.Length);
+            ShowPicture(0);
+        }
+
+        private void ChooseWord()
+        {
+            string[] nl = { "\n" };
+            string[] lines = Properties.Resources.файл_со_словами.Split(nl, StringSplitOptions.RemoveEmptyEntries);
+            do
+            {
+                word = lines[random.Next(0, lines.Length)];
+            } while (word.Length < 5);
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            int letterNumber = (int)((Button)sender).Tag;
+            HideLetter(letterNumber);
+            if(word.IndexOf(abc[letterNumber]) >= 0)
+            {
+                // if chosen letter exists
+                // show all the letters in a word
+                string showWord = "";
+                for (int i = 0; i < labelWord.Text.Length; i++)
+                {
+                    if(labelWord.Text[i] == '#')
+                    {
+                        if(word[i] == abc[letterNumber])
+                        {
+                            showWord += abc[letterNumber];
+                        }
+                        else
+                        {
+                            showWord += "#";
+                        }
+                    }
+                    else
+                    {
+                        showWord += labelWord.Text[i];
+                    }
+                }
+                labelWord.Text = showWord;
+                if(showWord.IndexOf('#') == -1)
+                {
+                    ShowWin();
+                    InitGame();
+                }
+            }
+            else
+            {
+                // these letters do not exist in a word
+                errors++;
+                ShowPicture(errors);
+                if(errors >= 7)
+                {
+                    ShowLose();
+                    InitGame();
+                }
+            }
+            if (textList.Visible)
+            {
+                RunViselkaHelper();
+            }
+        }
+
+        private void RunViselkaHelper()
+        {
+            textList.Text = "";
+            int[] used = new int[abc.Length];
+            string[] nl = { "\n" };
+            string[] words = Properties.Resources.файл_со_словами.Split(nl, StringSplitOptions.RemoveEmptyEntries);
+            string m = labelWord.Text;
+            StringBuilder sb = new StringBuilder();
+            int total = 0;
+
+            foreach (string word in words)
+            {
+                if(word.Length != m.Length)
+                {
+                    continue;
+                }
+
+                bool ok = true;
+
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if(m[i] == '#')
+                    {
+                        if (IsLetterFree(word[i]))
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            ok = false;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if(word[i] == m[i])
+                        {
+                            continue; 
+                        }
+                        else
+                        {
+                            ok = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (!ok)
+                {
+                    continue;
+                }
+
+                for (int i = 0; i < abc.Length; i++)
+                {
+                    if (word.Contains(abc[i].ToString()))
+                    {
+                        used[i]++;
+                    }
+                }
+
+                sb.AppendLine(word);
+                total++;
+            }
+
+            int max = 0;
+            int maxi = -1;
+            for (int i = 0; i < abc.Length; i++)
+            {
+                if (IsLetterFree(abc[i]))
+                {
+                    if(used[i] > max)
+                    {
+                        max = used[i];
+                        maxi = i;
+                    }
+                }
+            }
+            sb.AppendLine("Назовите букву " + abc[maxi]); 
+
+            textList.Text = "Найдено слов: " + total.ToString() + Environment.NewLine +
+                "Лучшая буква: " + abc[maxi] + Environment.NewLine +
+                "Использована: " +  used[maxi] + Environment.NewLine + 
+                sb.ToString();
+        }
+
+        private bool IsLetterFree(char v)
+        {
+            int i = abc.IndexOf(v);
+            return buttonKeys[i].Visible;
+        }
+
+        private void ShowLose()
+        {
+            MessageBox.Show("К сожалению вас повесили!\nЭто было слово " + word, "Вы проиграли");
+        }
+
+        private void ShowWin()
+        {
+            MessageBox.Show("Поздравляю! Вы угадали слово", "Вы выиграли");
+        }
+
+        private void ShowPicture(int number)
+        {
+            switch (number)
+            {
+                case 0: pictureStep.Image = Properties.Resources.step0; break;
+                case 1: pictureStep.Image = Properties.Resources.step1; break;
+                case 2: pictureStep.Image = Properties.Resources.step2; break;
+                case 3: pictureStep.Image = Properties.Resources.step3; break;
+                case 4: pictureStep.Image = Properties.Resources.step4; break;
+                case 5: pictureStep.Image = Properties.Resources.step5; break;
+                case 6: pictureStep.Image = Properties.Resources.step6; break;
+                case 7: pictureStep.Image = Properties.Resources.step7; break;
+                default: pictureStep.Image = null; break;
+            }
+        }
+
+        private void HideLetter(int number)
+        {
+            // to make buttons disappear
+            buttonKeys[number].Visible = false;
+        }
+
+        private void labelWord_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Right)
+            {
+                textList.Visible = !textList.Visible;
+            }
+        }
+    }
+}
